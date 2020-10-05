97c58c1d1d944dd8fbb4d56ad91a4d20c2ce2b6e
p-adams <p-adams@unknown9078413a1e98.attlocal.net>
2020-10-04 18:54:01 -0700

    remove props

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,15 +6,6 @@
 import { defineComponent } from "vue";
 import ResumeBuilder from "./ResumeBuilder.vue";
 export default defineComponent({
-  name: "HelloWorld",
   components: { ResumeBuilder },
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      count: 0,
-    };
-  },
 });
 </script>
